464160c040cf843c402114a1985f698beedf53c9
codeo <alphaCodeo@users.noreply.github.com>
2022-02-07 16:32:11 +0800

    Initial commit (working for 2 players Villager and Mafioso)

---
+++ precache-manifest.5abcd186bb83afd33aca0d67c41c14a8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "669acaf382bbd5cb9614e83eeeb0b416",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9083850c2b04135558f",
+    "url": "/static/js/2.736c2884.chunk.js"
+  },
+  {
+    "revision": "6e0da71d565667c291a2",
+    "url": "/static/js/main.75af5e73.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
